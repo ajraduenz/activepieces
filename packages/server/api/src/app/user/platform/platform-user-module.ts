c2ac6eccab4506e4f66d90385f78504a451e0950
Aldo Raduenz Junior <aldo.junior@wk.com.br>
2026-02-03 13:59:42 -0300

    commit inicial

--- packages/server/api/src/app/user/platform/platform-user-module.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
-import { platformUserController } from './platform-user-controller'
-
-export const platformUserModule: FastifyPluginAsyncTypebox = async (app) => {
-    await app.register(platformUserController, { prefix: '/v1/users' })
-}
